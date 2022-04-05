3744c53dbc4c611d7e88c77c35eb09c853a0c3d7
Felipe nascimento <89118274+flpente@users.noreply.github.com>
2022-04-05 16:48:10 -0300

    aplicação bem sucedida do gulp

--- projeto/gulpfile.js
+++ projeto/gulpfile.js
@@ -1,8 +1,23 @@
 const gulp = require('gulp')
+const concat = require('gulp-concat')
+const cssmin = require('gulp-cssmin')
+const uglify = require('gulp-uglify')
+const rename = require('gulp-rename')
 
 function tarefasCSS(cb){
     return gulp.src('./vendor/**/*.css')
-        .pipe(gulp.dest('./dist'))
+        .pipe(concat('libs.css'))
+        .pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('./dist/CSS'))
+}
+function tarefasJS(){
+    return gulp.src('./vendor/**/*.js')
+        .pipe(concat('libs.js'))
+        .pipe(uglify())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('./dist/JS'))
 }
 
 exports.styles = tarefasCSS
+exports.scripts = tarefasJS
