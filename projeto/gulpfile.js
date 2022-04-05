72426ea55450ea4103b5e1bdc554055d258fa168
Felipe nascimento <89118274+flpente@users.noreply.github.com>
2022-04-05 17:40:19 -0300

    adição do images com erro

--- projeto/gulpfile.js
+++ projeto/gulpfile.js
@@ -3,21 +3,53 @@ const concat = require('gulp-concat')
 const cssmin = require('gulp-cssmin')
 const uglify = require('gulp-uglify')
 const rename = require('gulp-rename')
+const image = require('gulp-image')
+const stripJs = require('gulp-strip-comments')
+const stripCss = require('gulp-strip-css-comments')
 
 function tarefasCSS(cb){
-    return gulp.src('./vendor/**/*.css')
-        .pipe(concat('libs.css'))
+    return gulp.src([
+        './node_modules/bootstrap/dist/css/bootstrap.css',
+        './node_modules/@fortawesome/fontawesome-free/css/fontawesome.css',
+        './vendor/owl/css/owl.css',
+        './src/css/style.css'
+    ])
+        .pipe(stripCss())
+        .pipe(concat('styles.css'))
         .pipe(cssmin())
         .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('./dist/CSS'))
 }
 function tarefasJS(){
-    return gulp.src('./vendor/**/*.js')
-        .pipe(concat('libs.js'))
+    return gulp.src([
+        './node_modules/jquery/dist/jquery.js',
+        './node_modules/bootstrap/dist/js/bootstrap.js',
+        './vendor/owl/js/owl.js',
+        './src/js/custom.js'
+    ])
+        .pipe(stripJs())
+        .pipe(concat('script.js'))
         .pipe(uglify())
         .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('./dist/JS'))
 }
+function tarefasImagens(){
+    return gulp.src('./src/images/*')
+        .pipe(image({
+            pngquant: true,
+            optipng: false,
+            zopflipng: true,
+            jpegRecompress: false,
+            mozjpeg: true,
+            gifsicle: true,
+            svgo: true,
+            concurrent: 10,
+            quiet: true
+        }))
+        .pipe(gulp.dest('./dist/images'))
+}
+
 
 exports.styles = tarefasCSS
 exports.scripts = tarefasJS
+exports.images = tarefasImagens
